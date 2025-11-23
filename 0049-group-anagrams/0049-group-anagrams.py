075b9617c0b2235d4d2c6fa4d5701be52a5155bd
HYUNMIN YOO <2049905@naver.com>
2025-11-23 16:53:30 +0900

    Time: 11 ms (83.19%), Space: 21.2 MB (42.6%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ana = dict()
+
+        for i in strs:
+            keys = tuple(sorted(i))
+            if keys not in ana:
+                ana[keys] = []
+            ana[keys].append(i)
+        return(list(ana.values()))
