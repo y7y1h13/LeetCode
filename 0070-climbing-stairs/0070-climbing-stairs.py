518a90d14d8daa6d2af018d14803639e967fb61b
HYUNMIN YOO <2049905@naver.com>
2026-02-17 14:56:37 +0900

    Time: 0 ms (100%), Space: 19.4 MB (39.73%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1] * (n + 1)
+        
+        def dfs(x):
+            if x <= 2:
+                return x
+            
+            if dp[x] != -1:
+                return dp[x]
+            
+            dp[x] = dfs(x - 1) + dfs(x - 2)
+            return dp[x]
+        
+        return dfs(n)
