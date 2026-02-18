8f144f0157cd0b4314b54754aa6ad7e7354e82e2
HYUNMIN YOO <2049905@naver.com>
2026-02-18 12:23:56 +0900

    Time: 0 ms (100%), Space: 19.6 MB (13.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,12 @@
+from functools import lru_cache
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [-1] * (n+1)
         
+        @lru_cache(None)
         def dfs(x):
             if x <= 2:
                 return x
-            if dp[x] != -1:
-                return dp[x]
-            dp[x] = dfs(x-1) + dfs(x-2)
-            return dp[x]
+            return dfs(x - 1) + dfs(x - 2)
+        
         return dfs(n)
